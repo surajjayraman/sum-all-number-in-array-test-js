463a8e5e4d6f438250ddcf02c80d10640695fcbb
surajjayraman <suraj.jayraman@gmail.com>
2023-06-03 18:40:44 -0400

    Update code to implement a recursive algorithm.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  let sum = 0;
+  array.forEach(element => {
+    if (Array.isArray(element)) {
+      sum = sum + sumItems(element);
+    } else {
+      sum = sum + element;
+    }
+       
+  });
+
+  return sum;
+};
 
 module.exports = sumItems;
